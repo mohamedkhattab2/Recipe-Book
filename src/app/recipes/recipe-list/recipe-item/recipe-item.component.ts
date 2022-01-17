9114802e0e0b128eedd5712c8f8dffbf3f98bcfa
mohamed <32775877+mohamedkhattab2@users.noreply.github.com>
2022-01-17 19:14:40 +0200

    Passing Dynamic Parameters to Links

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -8,5 +8,6 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
   ngOnInit(): void {}
 }
